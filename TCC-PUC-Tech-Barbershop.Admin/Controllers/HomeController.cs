822e16f95352cc4aa993c89aa115623fc188c2a1
Gustavo Mariano <gustavocmariano@hotmail.com>
2023-03-22 01:57:14 -0300

    Limpando o código e retirando view Privacy.

--- TCC-PUC-Tech-Barbershop.Admin/Controllers/HomeController.cs
+++ TCC-PUC-Tech-Barbershop.Admin/Controllers/HomeController.cs
@@ -2,31 +2,30 @@
 using System.Diagnostics;
 using TCC_PUC_Tech_Barbershop.Admin.Models;
 
-namespace TCC_PUC_Tech_Barbershop.Admin.Controllers
+namespace TCC_PUC_Tech_Barbershop.Admin.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult About()
+    {
+        return View();
+    }
 
-        public IActionResult About()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
