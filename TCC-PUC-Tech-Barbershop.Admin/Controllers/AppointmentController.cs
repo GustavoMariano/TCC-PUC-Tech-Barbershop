556b42b2098ec96817e8e08012177c415faa8b40
Gustavo Mariano <gustavocmariano@hotmail.com>
2023-04-03 02:54:27 -0300

    Implementado front de Login e Cadastrar, iniciando tela de agendamentos.

---
+++ TCC-PUC-Tech-Barbershop.Admin/Controllers/AppointmentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TCC_PUC_Tech_Barbershop.Admin.Controllers;
+
+public class AppointmentController : Controller
+{
+    public IActionResult BookAppointment()
+    {
+        return View();
+    }
+}
---
+++ TCC-PUC-Tech-Barbershop.Admin/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TCC_PUC_Tech_Barbershop.Admin.Controllers;
+
+public class LoginController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+}
